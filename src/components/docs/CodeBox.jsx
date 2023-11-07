be2b817bbe59c1e6502659f41630a7f5b46fe22f
dongggggae <wjdqls149@naver.com>
2023-11-07 23:55:49 +0900

    Markdown 패키지 추가

--- src/components/docs/CodeBox.jsx
+++ src/components/docs/CodeBox.jsx
@@ -1,12 +1,11 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const CodeBox = () => {
   const codeString = 'const export = () => {<SyntaxHighlighter language="jsx" style={dark}>{codeString}</SyntaxHighlighter>};';
 
   return (
-    <SyntaxHighlighter language="jsx" style={dark}>
+    <SyntaxHighlighter language="jsx" style={materialDark}>
       {codeString}
     </SyntaxHighlighter>
   );
