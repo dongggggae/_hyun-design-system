827945c7d7cfd0087f95d7e6bf9778563d55477e
dongggggae <wjdqls149@naver.com>
2023-11-11 17:29:04 +0900

    수정 - App.jsx 주석 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// import CodeContainer from './components/docs/CodeContainer';
-// import Heading from './components/hyun-design-system/typography/Heading';
-// import Text from './components/hyun-design-system/typography/Text';
-// import Button from './components/hyun-design-system/button/Button';
 import Layout from './layouts/Layout';
 
 import './assets/app.css';
