cb55484465b3bd9c270e9f67f8bce12261262aba
dongggggae <wjdqls149@naver.com>
2023-11-13 22:54:45 +0900

    Heading prop 설정 변경

--- src/components/hyun-design-system/typography/Heading.jsx
+++ src/components/hyun-design-system/typography/Heading.jsx
@@ -52,7 +52,7 @@ StyledHeading.defaultProps = {
 
 Heading.propTypes = {
   as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
-  size: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
+  size: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
   text: PropTypes.string.isRequired,
 };
 
