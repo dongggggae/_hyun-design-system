a5b30053bcd92934bf1882c2be6959c672bce733
김동현 <doyn0822@hiconsy.com>
2023-11-14 11:24:23 +0900

    파일 롤백

--- src/theme/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ThemeContext, getThemeContext, ThemeProvier, ThemeConsumer } from './src/ThemeContext';
-export { default as useTheme } from './src/useTheme';
--- src/theme/src/useTheme.js
+++
@@ -1,8 +0,0 @@
-import { ThemeContext } from '@emotion/react';
-import { useContext } from 'react';
-
-function useTheme() {
-  return useContext(ThemeContext);
-}
-
-export default useTheme;
