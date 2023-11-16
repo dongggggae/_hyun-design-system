46b94804cde533083b58a6a034ff7927fb73ad2f
김동현 <doyn0822@hiconsy.com>
2023-11-16 17:31:01 +0900

    Docs - Button 추가 업데이트

--- src/components/hyun-design-system/button/Button.jsx
+++ src/components/hyun-design-system/button/Button.jsx
@@ -8,14 +8,30 @@ const StyledButton = styled.button`
   ${(props) => props.theme.components.button.buttonTypes[props.type]}
 `;
 
-const Button = ({ type, size, text }) => {
+const Button = ({ type, size, text, children, reverse, onClick }) => {
   const PREFIX = 'Btn';
   const classNames = [PREFIX, `${PREFIX}-${type}`].join(' ');
   const theme = useTheme();
 
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <StyledButton className={classNames} type={type} size={size} theme={theme}>
-      {text}
+    <StyledButton className={classNames} type={type} size={size} theme={theme} onClick={handleClick}>
+      {reverse ? (
+        <>
+          {text}
+          {children}
+        </>
+      ) : (
+        <>
+          {children}
+          {text}
+        </>
+      )}
     </StyledButton>
   );
 };
@@ -23,12 +39,16 @@ const Button = ({ type, size, text }) => {
 Button.defaultProps = {
   type: 'solidGreen',
   size: 'md',
+  reverse: false,
 };
 
 Button.propTypes = {
   type: PropTypes.oneOf(['solidGreen', 'solidRed', 'outlineGreen', 'outlineGray']),
   size: PropTypes.oneOf(['lg', 'md', 'sm', 'xs']),
   text: PropTypes.string,
+  children: PropTypes.node,
+  reverse: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Button;
