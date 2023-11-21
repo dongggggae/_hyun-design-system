5e8d565637c59643cf4da8af617f359defa658d7
김동현 <doyn0822@hiconsy.com>
2023-11-21 10:54:39 +0900

    lintignore 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'prettier'],
-  ignorePatterns: ['dist', '.eslintrc.cjs', '*.md'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.md', 'index.html'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
