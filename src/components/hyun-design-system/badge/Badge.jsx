a457aae57e7a7f0d116808be82acfbb27dff53b2
김동현 <doyn0822@hiconsy.com>
2023-11-23 11:41:45 +0900

    Badge 스타일 추가,변경

--- src/components/hyun-design-system/badge/Badge.jsx
+++ src/components/hyun-design-system/badge/Badge.jsx
@@ -4,16 +4,18 @@ import styled from '@emotion/styled';
 import { useTheme } from '../../../theme/ThemeContext';
 
 const StyledBadge = styled.span`
-  ${(props) => props.theme.components.badge.baseStyle}
-  ${(props) => props.theme.components.badge.badgeTypes[props.state]}
+  ${(props) => props.theme.components.badge.baseStyle};
+  background-color: ${(props) => (props.outline ? 'white' : props.theme.components.badge.badgeTypes[props.state])};
+  border-color: ${(props) => (props.outline ? props.theme.components.badge.badgeTypes[props.state] : 'transparent')};
+  color: ${(props) => (props.outline ? props.theme.components.badge.badgeTypes[props.state] : 'white')};
 `;
 
-const Badge = React.memo(({ state, text }) => {
+const Badge = React.memo(({ state, text, outline }) => {
   const theme = useTheme();
   const PREFIX = 'badge';
 
   return (
-    <StyledBadge className={PREFIX} state={state} theme={theme}>
+    <StyledBadge className={PREFIX} state={state} outline={outline} theme={theme}>
       {text}
     </StyledBadge>
   );
@@ -21,11 +23,13 @@ const Badge = React.memo(({ state, text }) => {
 
 Badge.defaultProps = {
   state: 'information',
+  outline: false,
 };
 
 Badge.propTypes = {
   state: PropTypes.oneOf(['success', 'warn', 'error', 'information']),
   text: PropTypes.string,
+  outline: PropTypes.bool,
 };
 
 Badge.displayName = 'Badge';
