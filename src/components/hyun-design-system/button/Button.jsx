9180f9307de213cbb108bf1b09f837e8bb6f769a
dongggggae <wjdqls149@naver.com>
2023-11-25 19:06:39 +0900

    Button,Icon 컴포넌트 스타일 수정

--- src/components/hyun-design-system/button/Button.jsx
+++ src/components/hyun-design-system/button/Button.jsx
@@ -9,7 +9,13 @@ const StyledButton = styled.button`
   ${(props) => props.theme.components.button.buttonTypes[props.type]}
 `;
 
-const Button = React.memo(({ type, size, text, children, reverse, onClick }) => {
+const StyledIconButton = styled.button`
+  ${(props) => props.theme.components.button.baseStyle}
+  ${(props) => props.theme.components.button.iconButtonStyle}
+  ${(props) => props.theme.components.button.iconButtonSizes[props.size]}
+`;
+
+const Button = React.memo(({ type, size, text, children, reverse, icononly, onClick }) => {
   const PREFIX = 'btn';
   const theme = useTheme();
 
@@ -20,19 +26,29 @@ const Button = React.memo(({ type, size, text, children, reverse, onClick }) => 
   };
 
   return (
-    <StyledButton className={PREFIX} type={type} size={size} theme={theme} onClick={handleClick}>
-      {reverse ? (
-        <>
-          {text}
+    <React.Fragment>
+      {/*  아이콘 only 일때는 name을 그냥 Icon에 porps를 주고 + 새로운 스타일을 줘야함  + Icon에는 웹접근서성을
+      위해서 블라인드 메시지를 줘야하는데 이걸 그냥 js파일에서 관리하는게 나을 듯?*/}
+      {icononly ? (
+        <StyledIconButton className={`${PREFIX}__icon`} type={type} size={size} theme={theme} onClick={handleClick}>
           {children}
-        </>
+        </StyledIconButton>
       ) : (
-        <>
-          {children}
-          {text}
-        </>
+        <StyledButton className={PREFIX} type={type} size={size} theme={theme} onClick={handleClick}>
+          {reverse ? (
+            <>
+              {text}
+              {children}
+            </>
+          ) : (
+            <>
+              {children}
+              {text}
+            </>
+          )}
+        </StyledButton>
       )}
-    </StyledButton>
+    </React.Fragment>
   );
 });
 
@@ -40,6 +56,7 @@ Button.defaultProps = {
   type: 'solidGreen',
   size: 'md',
   reverse: false,
+  icononly: false,
 };
 
 Button.propTypes = {
@@ -48,6 +65,7 @@ Button.propTypes = {
   text: PropTypes.string,
   children: PropTypes.node,
   reverse: PropTypes.bool,
+  icononly: PropTypes.bool,
   onClick: PropTypes.func,
 };
 
--- src/components/hyun-design-system/input/InputAddon.jsx
+++ src/components/hyun-design-system/input/InputAddon.jsx
@@ -7,21 +7,18 @@ const InputAddon = React.memo(({ value, clear, field, handleClear, showPassword,
   return (
     <div className="input-wrap__addon">
       {clear && value.length > 0 && (
-        <Button size="xs" onClick={handleClear}>
+        <Button icononly size="xs" onClick={handleClear}>
           <Icon name="closeFill" size="sm" />
-          <b className="blind">입력취소</b>
         </Button>
       )}
       {field === 'password' && (
-        <Button size="xs" onClick={handleTogglePassword}>
+        <Button icononly size="xs" onClick={handleTogglePassword}>
           <Icon name={showPassword ? 'invisible' : 'visible'} size="sm" />
-          <b className="blind">{showPassword ? '비밀번호 보기' : '비밀번호 숨기기'}</b>
         </Button>
       )}
       {field === 'search' && (
-        <Button size="xs">
+        <Button icononly size="xs">
           <Icon name="search" size="sm" />
-          <b className="blind">검색</b>
         </Button>
       )}
     </div>
