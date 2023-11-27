d89994d8b6e5ccb61032eee6a904673553fe417e
김동현 <doyn0822@hiconsy.com>
2023-11-27 14:23:06 +0900

    lint - import/order 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,39 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'prettier'],
-  ignorePatterns: ['dist', '.eslintrc.cjs', '*.md', 'index.html'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.md', 'index.html', '*config.js'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       {
         allowConstantExport: true,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
       },
     ],
   },
