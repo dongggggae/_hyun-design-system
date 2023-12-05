c4d1d6d7f76772db35e3a4bb0a9f205a4d135955
김동현 <doyn0822@hiconsy.com>
2023-12-05 12:15:46 +0900

    Docs - table 작업중

--- src/pages/components/TablePage.jsx
+++ src/pages/components/TablePage.jsx
@@ -9,11 +9,11 @@ const LazyCodeContainer = lazy(() => import('../../components/docs/CodeContainer
 const ComponentPage = () => {
   return (
     <div className="docs">
-      <div className="docs-button">
+      <div className="docs-table">
         <div className="component-content">
           <Heading as="h2" size="2" text="1. 개요" />
           <Text>Table 컴포넌트를 통해 테이블 요소를 생성합니다.</Text>
-          <div className="preview">
+          <div className="preview fill">
             <Table>
               <TableHead>
                 <TableRow>
@@ -30,18 +30,49 @@ const ComponentPage = () => {
                 </TableRow>
                 <TableRow>
                   <TableData>Data1</TableData>
-                  <TableData colSpan={2}>Data2</TableData>
+                  <TableData>Data2</TableData>
+                  <TableData>Data3</TableData>
                 </TableRow>
                 <TableRow>
-                  <TableData colSpan={2}>Data1</TableData>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                  <TableData>Data3</TableData>
+                </TableRow>
+              </TableBody>
+            </Table>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableHeader>Header1</TableHeader>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>Header2</TableHeader>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>Header2</TableHeader>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableHeader>Header2</TableHeader>
+                  <TableData>Data1</TableData>
                   <TableData>Data2</TableData>
                 </TableRow>
               </TableBody>
             </Table>
           </div>
-          <Suspense fallback={<div>Loading...</div>}>
-            <LazyCodeContainer codeString={TableCode.overview} />
-          </Suspense>
+          <div className='code-box'>
+            <Suspense fallback={<div>Loading...</div>}>
+              <LazyCodeContainer codeString={TableCode.overview} />
+            </Suspense>
+            <Suspense fallback={<div>Loading...</div>}>
+              <LazyCodeContainer codeString={TableCode.overview2} />
+            </Suspense>
+          </div>
         </div>
 
         <div className="component-content">
@@ -77,6 +108,42 @@ const ComponentPage = () => {
             <LazyCodeContainer codeString={TableCode.span} />
           </Suspense>
         </div>
+
+        <div className="component-content">
+          <Heading as="h2" size="2" text="2. active" />
+          <Text>TableRow 컴포넌트에 active 프로퍼티를 통해 선택한 스타일을 적용할 수 있습니다.</Text>
+          <div className="preview">
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableHeader>Header1</TableHeader>
+                  <TableHeader>Header2</TableHeader>
+                  <TableHeader>Header3</TableHeader>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow active>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                  <TableData>Data3</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                  <TableData>Data3</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableData>Data1</TableData>
+                  <TableData>Data2</TableData>
+                  <TableData>Data3</TableData>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyCodeContainer codeString={TableCode.span} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
@@ -100,14 +167,46 @@ const TableCode = {
     `   </TableRow>`,
     `   <TableRow>`,
     `     <TableData>Data1</TableData>`,
-    `     <TableData colSpan={2}>Data2</TableData>`,
+    `     <TableData>Data2</TableData>`,
+    `     <TableData>Data3</TableData>`,
     `   </TableRow>`,
     `   <TableRow>`,
-    `     <TableData colSpan={2}>Data1</TableData>`,
+    `     <TableData>Data1</TableData>`,
+    `     <TableData>Data2</TableData>`,
+    `     <TableData>Data3</TableData>`,
+    `   </TableRow>`,
+    ` </TableBody>`,
+    `</Table>`,
+  ].join(`\n`),
+  overview2: [
+    ``,
+    `<Table>`,
+    ` <TableBody>`,
+    `   <TableRow>`,
+    `     <TableHeader>Header1</TableHeader>`,
+    `     <TableData>Data1</TableData>`,
+    `     <TableData>Data2</TableData>`,
+    `   </TableRow>`,
+    `   <TableRow>`,
+    `     <TableHeader>Header2</TableHeader>`,
+    `     <TableData>Data1</TableData>`,
+    `     <TableData>Data2</TableData>`,
+    `   </TableRow>`,
+    `   <TableRow>`,
+    `     <TableHeader>Header2</TableHeader>`,
+    `     <TableData>Data1</TableData>`,
+    `     <TableData>Data2</TableData>`,
+    `   </TableRow>`,
+    `   <TableRow>`,
+    `     <TableHeader>Header2</TableHeader>`,
+    `     <TableData>Data1</TableData>`,
     `     <TableData>Data2</TableData>`,
     `   </TableRow>`,
     ` </TableBody>`,
     `</Table>`,
+    ``,
+    ``,
+    
   ].join(`\n`),
   span: [
     `<Table>`,
