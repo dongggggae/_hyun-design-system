1c6b2f000fec65e1ec7080dac02034bb4627d76f
김동현 <doyn0822@hiconsy.com>
2023-12-07 14:17:01 +0900

    Component - Table 수정

--- src/components/hyun-design-system/table/Table.jsx
+++ src/components/hyun-design-system/table/Table.jsx
@@ -1,27 +1,39 @@
+import { createContext, useContext } from 'react';
+
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 import { useTheme } from '../../../theme/ThemeContext';
 
+const TableContext = createContext();
+
 const StyledTable = styled.table`
   ${(props) => props.theme.components.table.baseStyle}
 `;
 
-const Table = ({ children }) => {
+const Table = ({ children, hover, type }) => {
   const PREFIX = 'table';
   const theme = useTheme();
 
   return (
-    <StyledTable className={PREFIX} theme={theme}>
-      {children}
-    </StyledTable>
+    <TableContext.Provider value={{ hover, type }}>
+      <StyledTable className={PREFIX} theme={theme}>
+        {children}
+      </StyledTable>
+    </TableContext.Provider>
   );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTable = () => {
+  return useContext(TableContext);
 };
 
 Table.propTypes = {
   children: PropTypes.node,
   type: PropTypes.string,
   active: PropTypes.bool,
+  hover: PropTypes.bool,
 };
 
 export default Table;
