c98468776c976692f6a4f8d729c0f2734b10f395
김동현 <doyn0822@hiconsy.com>
2023-12-15 17:10:53 +0900

    [DS-28]Docs - Btn 추가

--- src/pages/components/ButtonPage.jsx
+++ src/pages/components/ButtonPage.jsx
@@ -48,10 +48,10 @@ const ComponentPage = () => {
             size 프로퍼티 속성은 &#39;lg&#39;, &#39;md&#39;, &#39;sm&#39; &#39;xs&#39; 가 있습니다.
           </Text>
           <div className="preview">
-            <Btn text="SolidGreen Button" size="xs" />
-            <Btn text="SolidGreen Button" size="sm" />
-            <Btn text="SolidGreen Button" />
             <Btn text="SolidGreen Button" size="lg" />
+            <Btn text="SolidGreen Button" />
+            <Btn text="SolidGreen Button" size="sm" />
+            <Btn text="SolidGreen Button" size="xs" />
           </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.size} />
@@ -65,7 +65,14 @@ const ComponentPage = () => {
             <br />
             reverse 프로퍼티를 통해 Icon 과 Text의 위치를 변경할 수 있습니다. 기본값은 &#39;false&#39;입니다.
           </Text>
-          <div className="preview"></div>
+          <div className="preview">
+            <Btn iconOnly icon="plus" />
+            <Btn type="solid-green" text="Button Text" icon="plus" />
+            <Btn type="solid-green" text="Button Text" reverse icon="plus" />
+            <Btn type="solid-red" iconOnly icon="plus" />
+            <Btn type="solid-red" text="Button Text" icon="plus" />
+            <Btn type="solid-red" text="Button Text" reverse icon="plus" onClick={() => alert('Click Event')} />
+          </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.icon} />
           </Suspense>
@@ -74,7 +81,9 @@ const ComponentPage = () => {
         <div className="component-content">
           <Heading as="h2" size="2" text="5. Events" />
           <Text>onClick 프로퍼티를 통해 클릭 이벤트를 발생시킵니다.</Text>
-          <div className="preview"></div>
+          <div className="preview">
+            <Btn text="Click Button" onClick={() => alert('Click Event')} />
+          </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.event} />
           </Suspense>
@@ -103,47 +112,41 @@ const ButtonCode = {
   ].join(`\n`),
   type: [
     `<!-- SolidGreen Button> -->`,
-    `<Button text="SolidGreen Button" />`,
+    `<Btn text="SolidGreen Button" />`,
     ``,
     `<!-- SolidRed Button> -->`,
-    `<Button type="solidRed" text="SolidRed Button" />`,
+    `<Btn type="solidRed" text="SolidRed Button" />`,
     ``,
     `<!-- OutlineGreen Button> -->`,
-    `<Button type="outlineGreen" text="OutlineGreen Button" />`,
+    `<Btn type="outlineGreen" text="OutlineGreen Button" />`,
     ``,
     `<!-- OutlineGray Button> -->`,
-    `<Button type="outlineGray" text="OutlineGray Button" />`,
+    `<Btn type="outlineGray" text="OutlineGray Button" />`,
   ].join(`\n`),
   size: [
     `<!-- Large Button> -->`,
-    `<Button size="lg" text="Large Button" />`,
+    `<Btn size="lg" text="Large Button" />`,
     ``,
     `<!-- Medium Button> -->`,
-    `<Button text="Medium Button" />`,
+    `<Btn text="Medium Button" />`,
     ``,
     `<!-- Small Button> -->`,
-    `<Button size="sm" text="Small Button" />`,
+    `<Btn size="sm" text="Small Button" />`,
     ``,
     `<!-- XSmall Button> -->`,
-    `<Button size="xs" text="XSmall Button" />`,
+    `<Btn size="xs" text="XSmall Button" />`,
   ].join(`\n`),
   icon: [
     `<!-- Only Icon Button> -->`,
-    `<Button icononly>`,
-    `<Icon name="plus" color="white" />`,
-    `</Button>`,
+    `<Btn iconOnly icon="plus" />`,
     ``,
     `<!-- Left Icon Button> -->`,
-    `<Button text="Left Icon Button">`,
-    `<Icon name="plus" color="white" />`,
-    `</Button>`,
+    `<Btn text="ButtonText" icon="plus" />`,
     ``,
     `<!-- Right Icon Button> -->`,
-    `<Button text="Right Icon Button" reverse>`,
-    `<Icon name="plus" color="white" />`,
-    `</Button>`,
+    `<Btn type="solid-red" text="ButtonText" reverse icon="plus" />`,
   ].join(`\n`),
-  event: [`<Button text="Click Button" onClick={() => alert('Click Event')} />`].join(`\n`),
+  event: [`<Btn text="Click Button" onClick={() => alert('Click Event')} />`].join(`\n`),
 };
 
 export default ComponentPage;
