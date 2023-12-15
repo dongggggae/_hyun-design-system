87a1847a938bad9ac0c42a6f8fc36f8323ca6f9d
김동현 <doyn0822@hiconsy.com>
2023-12-15 15:48:31 +0900

    [feature/DS-6]Components - Btn 기본 타입 추가

--- src/pages/components/ButtonPage.jsx
+++ src/pages/components/ButtonPage.jsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from 'react';
 
-import Button from '../../components/hyun-design-system/button/Button';
-import Icon from '../../components/hyun-design-system/icon/Icon';
+import Btn from '../../components/hyun-design-system/button/Button';
 import Heading from '../../components/hyun-design-system/typography/Heading';
 import Text from '../../components/hyun-design-system/typography/Text';
 
@@ -13,12 +12,13 @@ const ComponentPage = () => {
       <div className="docs-button">
         <div className="component-content">
           <Heading as="h2" size="2" text="1. 개요" />
-          <Text>Button 컴포넌트를 통해 버튼 요소를 생성합니다.</Text>
+          <Text>Btn 컴포넌트를 통해 버튼 요소를 생성합니다.</Text>
           <div className="preview">
-            <Button type="solidGreen" text="SolidGreen Button"></Button>
-            <Button type="solidRed" text="SolidRed Button"></Button>
-            <Button type="outlineGreen" text="OutlineGreen Button"></Button>
-            <Button type="outlineGray" text="OutlineGray Button"></Button>
+            <Btn text="SolidGreen Button" />
+            <Btn type="solid-red" text="SolidRed Button" />
+            <Btn type="outline-green" text="OutLinGreen Button" />
+            <Btn type="outline-gray" text="OutLinGray Button" />
+            <Btn type="link" text="Link Button" />
           </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.overview} />
@@ -27,12 +27,13 @@ const ComponentPage = () => {
 
         <div className="component-content">
           <Heading as="h2" size="2" text="2. Style" />
-          <Text>버튼 스타일은 type 프로퍼티를 통해 변경합니다. 기본 값은 &#39;solidGreen&#39; 입니다.</Text>
+          <Text>버튼 스타일은 type 프로퍼티를 통해 변경합니다. 기본 값은 &#39;solid-green&#39; 입니다.</Text>
           <div className="preview">
-            <Button type="solidGreen" text="SolidGreen Button"></Button>
-            <Button type="solidRed" text="SolidRed Button"></Button>
-            <Button type="outlineGreen" text="OutlineGreen Button"></Button>
-            <Button type="outlineGray" text="OutlineGray Button"></Button>
+            <Btn text="SolidGreen Button" />
+            <Btn type="solid-red" text="SolidRed Button" />
+            <Btn type="outline-green" text="OutLinGreen Button" />
+            <Btn type="outline-gray" text="OutLinGray Button" />
+            <Btn type="link" text="Link Button" />
           </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.type} />
@@ -47,10 +48,10 @@ const ComponentPage = () => {
             size 프로퍼티 속성은 &#39;lg&#39;, &#39;md&#39;, &#39;sm&#39; &#39;xs&#39; 가 있습니다.
           </Text>
           <div className="preview">
-            <Button size="lg" text="Large Button" />
-            <Button text="Medium Button" />
-            <Button size="sm" text="Small Button" />
-            <Button size="xs" text="XSmall Button" />
+            <Btn text="SolidGreen Button" size="xs" />
+            <Btn text="SolidGreen Button" size="sm" />
+            <Btn text="SolidGreen Button" />
+            <Btn text="SolidGreen Button" size="lg" />
           </div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.size} />
@@ -64,17 +65,7 @@ const ComponentPage = () => {
             <br />
             reverse 프로퍼티를 통해 Icon 과 Text의 위치를 변경할 수 있습니다. 기본값은 &#39;false&#39;입니다.
           </Text>
-          <div className="preview">
-            <Button icononly>
-              <Icon name="plus" color="white" />
-            </Button>
-            <Button text="Left Icon Button">
-              <Icon name="plus" color="white" />
-            </Button>
-            <Button text="Right Icon Button" reverse>
-              <Icon name="plus" color="white" />
-            </Button>
-          </div>
+          <div className="preview"></div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.icon} />
           </Suspense>
@@ -83,9 +74,7 @@ const ComponentPage = () => {
         <div className="component-content">
           <Heading as="h2" size="2" text="5. Events" />
           <Text>onClick 프로퍼티를 통해 클릭 이벤트를 발생시킵니다.</Text>
-          <div className="preview">
-            <Button text="Click Button" onClick={() => alert('Click Event')} />
-          </div>
+          <div className="preview"></div>
           <Suspense fallback={<div>Loading...</div>}>
             <LazyCodeContainer codeString={ButtonCode.event} />
           </Suspense>
@@ -98,16 +87,19 @@ const ComponentPage = () => {
 const ButtonCode = {
   overview: [
     `<!-- SolidGreen Button> -->`,
-    `<Button text="SolidGreen Button" />`,
+    `<Btn text="SolidGreen Button" />`,
     ``,
     `<!-- SolidRed Button> -->`,
-    `<Button type="solidRed" text="SolidRed Button" />`,
+    `<Btn type="solid-red" text="SolidRed Button" />`,
     ``,
     `<!-- OutlineGreen Button> -->`,
-    `<Button type="outlineGreen" text="OutlineGreen Button" />`,
+    `<Btn type="outline-green" text="OutlineGreen Button" />`,
     ``,
     `<!-- OutlineGray Button> -->`,
-    `<Button type="outlineGray" text="OutlineGray Button" />`,
+    `<Btn type="outline-gray" text="OutlineGray Button" />`,
+    ``,
+    `<!-- Link Button> -->`,
+    `<Btn type="link" text="Link Button" />`,
   ].join(`\n`),
   type: [
     `<!-- SolidGreen Button> -->`,
