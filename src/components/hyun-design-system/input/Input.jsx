34337d087fb643d2622fc3478e5347834742e683
김동현 <doyn0822@hiconsy.com>
2023-12-15 15:47:52 +0900

    [feature/DS-6]버튼 수정으로 임시 주석 처리

--- src/components/hyun-design-system/input/Input.jsx
+++ src/components/hyun-design-system/input/Input.jsx
@@ -1,69 +1,69 @@
-import React, { useRef, useState, useCallback } from 'react';
+// import React, { useRef, useState, useCallback } from 'react';
 
-import styled from '@emotion/styled';
-import PropTypes from 'prop-types';
+// import styled from '@emotion/styled';
+// import PropTypes from 'prop-types';
 
-import InputAddon from './InputAddon';
+// import InputAddon from './InputAddon';
 
-const StyledInput = styled.div`
-  // .input-wrap__addon {
-  //   visibility: ${(props) => (props.hasValue ? 'visible' : 'hidden')};
-  // }
-`;
+// const StyledInput = styled.div`
+//   // .input-wrap__addon {
+//   //   visibility: ${(props) => (props.hasValue ? 'visible' : 'hidden')};
+//   // }
+// `;
 
-const Input = React.memo(({ label, field, clear, placeholder }) => {
-  const formInput = useRef();
-  const [value, setValue] = useState('');
-  const [type, setType] = useState(field === 'password' ? 'password' : 'text');
-  const [showPassword, setShowPassword] = useState(false);
+// const Input = React.memo(({ label, field, clear, placeholder }) => {
+//   const formInput = useRef();
+//   const [value, setValue] = useState('');
+//   const [type, setType] = useState(field === 'password' ? 'password' : 'text');
+//   const [showPassword, setShowPassword] = useState(false);
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+//   const handleChange = (e) => {
+//     setValue(e.target.value);
+//   };
 
-  const handleClear = useCallback(() => {
-    formInput.current.value = '';
-    setValue('');
-  }, []);
+//   const handleClear = useCallback(() => {
+//     formInput.current.value = '';
+//     setValue('');
+//   }, []);
 
-  const handleTogglePassword = () => {
-    setShowPassword(!showPassword);
-    setType(type === 'text' ? 'password' : 'text');
-  };
+//   const handleTogglePassword = () => {
+//     setShowPassword(!showPassword);
+//     setType(type === 'text' ? 'password' : 'text');
+//   };
 
-  return (
-    <React.Fragment>
-      {label && (
-        <div className="form-wrap">
-          <label className="form__label">{label}</label>
-          <StyledInput className="input-wrap" hasValue={value.length > 0}>
-            <input type={type} className="form__control" placeholder={placeholder} ref={formInput} onChange={handleChange} />
-            <InputAddon field={field} clear={clear} handleClear={handleClear} showPassword={showPassword} handleTogglePassword={handleTogglePassword} value={value} />
-          </StyledInput>
-        </div>
-      )}
-      {!label && (
-        <StyledInput className="input-wrap" hasValue={value.length > 0}>
-          <input type={type} className="form__control" placeholder={placeholder} ref={formInput} onChange={handleChange} />
-          <InputAddon field={field} clear={clear} handleClear={handleClear} showPassword={showPassword} handleTogglePassword={handleTogglePassword} value={value} />
-        </StyledInput>
-      )}
-    </React.Fragment>
-  );
-});
+//   return (
+//     <React.Fragment>
+//       {label && (
+//         <div className="form-wrap">
+//           <label className="form__label">{label}</label>
+//           <StyledInput className="input-wrap" hasValue={value.length > 0}>
+//             <input type={type} className="form__control" placeholder={placeholder} ref={formInput} onChange={handleChange} />
+//             <InputAddon field={field} clear={clear} handleClear={handleClear} showPassword={showPassword} handleTogglePassword={handleTogglePassword} value={value} />
+//           </StyledInput>
+//         </div>
+//       )}
+//       {!label && (
+//         <StyledInput className="input-wrap" hasValue={value.length > 0}>
+//           <input type={type} className="form__control" placeholder={placeholder} ref={formInput} onChange={handleChange} />
+//           <InputAddon field={field} clear={clear} handleClear={handleClear} showPassword={showPassword} handleTogglePassword={handleTogglePassword} value={value} />
+//         </StyledInput>
+//       )}
+//     </React.Fragment>
+//   );
+// });
 
-Input.defaultProps = {
-  clear: false,
-  field: 'text',
-};
+// Input.defaultProps = {
+//   clear: false,
+//   field: 'text',
+// };
 
-Input.propTypes = {
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
-  field: PropTypes.oneOf(['text', 'password', 'search']),
-  clear: PropTypes.bool,
-};
+// Input.propTypes = {
+//   label: PropTypes.string,
+//   placeholder: PropTypes.string,
+//   field: PropTypes.oneOf(['text', 'password', 'search']),
+//   clear: PropTypes.bool,
+// };
 
-Input.displayName = 'Input';
+// Input.displayName = 'Input';
 
-export default Input;
+// export default Input;
--- src/components/hyun-design-system/input/InputAddon.jsx
+++ src/components/hyun-design-system/input/InputAddon.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
+// import React from 'react';
 
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import Button from '../button/Button';
-import Icon from '../icon/Icon';
+// import Button from '../button/Button';
+// import Icon from '../icon/Icon';
 
-const InputAddon = React.memo(({ value, clear, field, handleClear, showPassword, handleTogglePassword }) => {
-  return (
-    <div className="input-wrap__addon">
-      {clear && value.length > 0 && (
-        <Button icononly size="xs" onClick={handleClear}>
-          <Icon name="closeFill" size="sm" />
-        </Button>
-      )}
-      {field === 'password' && (
-        <Button icononly size="xs" onClick={handleTogglePassword}>
-          <Icon name={showPassword ? 'invisible' : 'visible'} size="sm" />
-        </Button>
-      )}
-      {field === 'search' && (
-        <Button icononly size="xs">
-          <Icon name="search" size="sm" />
-        </Button>
-      )}
-    </div>
-  );
-});
+// const InputAddon = React.memo(({ value, clear, field, handleClear, showPassword, handleTogglePassword }) => {
+//   return (
+//     <div className="input-wrap__addon">
+//       {clear && value.length > 0 && (
+//         <Button icononly size="xs" onClick={handleClear}>
+//           <Icon name="closeFill" size="sm" />
+//         </Button>
+//       )}
+//       {field === 'password' && (
+//         <Button icononly size="xs" onClick={handleTogglePassword}>
+//           <Icon name={showPassword ? 'invisible' : 'visible'} size="sm" />
+//         </Button>
+//       )}
+//       {field === 'search' && (
+//         <Button icononly size="xs">
+//           <Icon name="search" size="sm" />
+//         </Button>
+//       )}
+//     </div>
+//   );
+// });
 
-InputAddon.propTypes = {
-  clear: PropTypes.bool,
-  field: PropTypes.string,
-  handleClear: PropTypes.func,
-  showPassword: PropTypes.bool,
-  handleTogglePassword: PropTypes.func,
-  value: PropTypes.string,
-};
+// InputAddon.propTypes = {
+//   clear: PropTypes.bool,
+//   field: PropTypes.string,
+//   handleClear: PropTypes.func,
+//   showPassword: PropTypes.bool,
+//   handleTogglePassword: PropTypes.func,
+//   value: PropTypes.string,
+// };
 
-InputAddon.displayName = 'InputAddon';
+// InputAddon.displayName = 'InputAddon';
 
-export default InputAddon;
+// export default InputAddon;
