1edf9c6fef95d2849bd1caeeb401a4bd3863775f
dongggggae <wjdqls149@naver.com>
2024-01-08 22:39:53 +0900

    [DS-56]Conponent - Modal(Scroll, fullscreen Type) 추가

--- src/components/hyun-design-system/modal/Dialog.jsx
+++ src/components/hyun-design-system/modal/Dialog.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 
+import { useModal } from './Modal';
+
 const Dialog = ({ children }) => {
   const PREFIX = 'modal';
-  return <div className={`${PREFIX}__dialog`}>{children}</div>;
+  const { size, type } = useModal();
+
+  return <div className={`${PREFIX}__dialog ${size ? `${PREFIX}__dialog--${size}` : ''} ${type ? `${PREFIX}__dialog--${type}` : ''}`}>{children}</div>;
 };
 
 Dialog.propTypes = {
