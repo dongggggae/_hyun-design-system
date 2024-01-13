f7b642511be29891a7ca61f1303a330c185f0078
dongggggae <wjdqls149@naver.com>
2024-01-13 18:07:51 +0900

    [DS-61]Component - Card 수정

---
+++ src/components/hyun-design-system/card/index.js
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { default as CardTitle } from './Title';
+export { default as CardText } from './Text';
+export { default as CardThumbnail } from './Thumbnail';
