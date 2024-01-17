65a616d1576660cadb98bd0031d80a36bb7d64d6
dongggggae <wjdqls149@naver.com>
2024-01-17 23:00:47 +0900

    [DS-72]Component - Icon 수정

--- src/components/hyun-design-system/Icon/Icon.jsx
+++ src/components/hyun-design-system/Icon/Icon.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
-
-import { useTheme } from '@/theme/ThemeContext';
-
-const StyledIcon = styled.span`
-  ${(props) => props.theme.components.icon.baseStyle}
-
-  .icon {
-    ${(props) => props.theme.components.icon.iconSizes[props.size]}
-    color:${(props) => props.theme.colors[props.color]};
-  }
-`;
 
 const Icon = React.memo(({ name, size, color }) => {
   const PREFIX = 'icon';
-  const theme = useTheme();
-  const SelectedIcon = theme.components.icon.iconTypes[name];
-  const SelectedIconName = theme.components.icon.iconNames[name];
-
-  if (!SelectedIcon) {
-    return null;
-  }
 
   return (
-    <StyledIcon size={size} theme={theme} color={color}>
-      <SelectedIcon className={PREFIX} />
-      <b className="blind">{SelectedIconName}</b>
-    </StyledIcon>
+    <i className={[PREFIX, `${PREFIX}__${name}`, `${PREFIX}--${size}`, `${PREFIX}--${color}`].join(' ')}>
+      <b className="blind">{name}</b>
+    </i>
   );
 });
 
 Icon.defaultProps = {
   size: 'md',
-  color: 'white',
+  color: 'black',
 };
 
 Icon.propTypes = {
